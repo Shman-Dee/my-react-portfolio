4909e6a2107b56aa24123bc3a16fd839f2e5ce20
Andrew McInally <realfunnyestate@yahoo.com>
2022-06-23 17:45:53 -0600

    added bootstrap

---
+++ client/src/components/home/Profile.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <a href="google.com">
+              <i className="fa fa-twitter-square"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile
